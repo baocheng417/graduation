740f2f46bddbee698ef21b9dc44fd492ddb5c3d4
computer <1029071214@qq.com>
2016-12-22 10:23:32 +0800

    12.21

---
+++ src/wuHuberman/PointVoltage.java
@@ -0,0 +1,21 @@
+package wuHuberman;
+
+public class PointVoltage
+{
+	private float voltage;
+
+	public PointVoltage(float voltage)
+	{
+		this.voltage = voltage;
+	}
+
+	public float getVoltage()
+	{
+		return voltage;
+	}
+
+	public void setVoltage(float voltage)
+	{
+		this.voltage = voltage;
+	}
+}
